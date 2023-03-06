80e52526007612a2fa42a80198ad55c916efe49c
MaRau12 <marthaelisabeth94@gmail.com>
2023-03-06 13:04:13 +0000

    card has props now

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Jumbotron = () => {
+export const Jumbotron = (props) => {
 	return(
 	<div className="jumbotron container border border-secondary  p-5">
-  		<h1 className="display-4">A Warm Welcome!</h1>
+  		<h1 className="display-4">{props.title}</h1>
   		<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   		<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
   		<a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
